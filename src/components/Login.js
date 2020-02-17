37fbe2877fa3494fa1160d9c0294df3f2910ec36
Verner Codoceo <vacodoceo@uc.cl>
2020-02-17 02:59:59 -0300

    Add material ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import { Button } from "@material-ui/core";
 
 class Login extends Component {
   render() {
     const { signInWithGoogle } = this.props;
     return (
       <div>
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <Button variant="contained" onClick={signInWithGoogle}>
+          Sign in with Google
+        </Button>
       </div>
     );
   }
