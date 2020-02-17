bbdddcf42d0d4d169b618c431ff2ae1cd5f9dfb1
Verner Codoceo <vacodoceo@uc.cl>
2020-02-17 04:20:16 -0300

    Add Real Time DB write and read

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { Container } from "@material-ui/core";
+import MaterialTable from "material-table";
 
 class ExpensesList extends Component {
   render() {
-    return <p>Lista</p>;
+    const { expenses } = this.props;
+    console.log(expenses);
+    return (
+      <div style={{ maxWidth: "90%" }}>
+        <MaterialTable
+          columns={[
+            { title: "Fecha", field: "date", type: "date" },
+            { title: "Autor", field: "author" },
+            { title: "Tipo", field: "type" },
+            { title: "Monto", field: "amount", type: "numeric" }
+          ]}
+          data={expenses}
+          title="Demo Title"
+        />
+      </div>
+    );
   }
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,75 @@
 import React, { Component } from "react";
+import RegisterExpense from "./RegisterExpense";
+import ExpensesList from "./ExpensesList";
+import Firebase from "firebase";
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: undefined,
+      type: "Bencina",
+      expenses: []
+    };
+
+    this.writeExpensesData = this.writeExpensesData.bind(this);
+    this.handleAmountChange = this.handleAmountChange.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+  }
+
+  handleAmountChange = e => {
+    const amount = e.target.value;
+    this.setState({ amount });
+  };
+
+  handleTypeChange = e => {
+    const type = e.target.value;
+    this.setState({ type });
+  };
+
+  writeExpensesData = () => {
+    let { expenses } = this.state;
+    expenses.push({
+      amount: this.state.amount,
+      type: this.state.type,
+      author: this.props.user.displayName,
+      date: new Date()
+    });
+    Firebase.database()
+      .ref("/")
+      .set(expenses);
+    console.log("DATA SAVED");
+  };
+
+  getExpensesData = () => {
+    let ref = Firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const expenses = snapshot.val();
+      this.setState({ expenses });
+    });
+    console.log("DATA RETRIEVED");
+  };
+
+  componentDidMount() {
+    this.getExpensesData();
+  }
+
   render() {
+    const { amount, type } = this.state;
     const { user, signOut } = this.props;
-    console.log(this.props);
     return (
       <div>
         <p>Bienvenido {user.displayName}</p>
         <button onClick={signOut}>Sign out</button>
+        <RegisterExpense
+          onSubmit={this.writeExpensesData}
+          amount={amount}
+          type={type}
+          handleAmountChange={this.handleAmountChange}
+          handleTypeChange={this.handleTypeChange}
+        />
+        <ExpensesList expenses={this.state.expenses} />
       </div>
     );
   }
